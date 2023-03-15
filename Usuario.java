d515c8a75c440d9952ade2cf4e44a13e2eb2edfc
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-03-15 00:32:53 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -8,15 +8,12 @@ public class Usuario {
 		
 		smartTv.diminuirVolume();
 		smartTv.diminuirVolume();
-		smartTv.diminuirVolume();
 		smartTv.aumentarVolume();
-		
 		smartTv.mudarCanal(13);
+		
 		System.out.println("Canal Atual : " + smartTv.canal);
 		System.out.println("Volume Atual : " + smartTv.volume);
 		System.out.println("TV Ligada ? " + smartTv.ligada);
-		System.out.println("Canal Atual : " + smartTv.canal);
-		System.out.println("Volume Atual : " + smartTv.volume);
 		
 		smartTv.ligar ();
 		System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
